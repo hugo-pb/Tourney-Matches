83fb9cdd21a1913c02f0fc6dff8ddd002bfda0b1
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-02 17:23:23 -0600

    created basic components

---
+++ src/components /Playerlist.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Playerlist = () => {
+  return (
+    <section className="PlayerList">
+      <h1>Current participating players</h1>
+      {/* Players will be shown here */}
+    </section>
+  );
+};
+
+export default Playerlist;
