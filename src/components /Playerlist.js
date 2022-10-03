9ff2b0d2e0339aa4abda08991581eba5110cfc58
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-02 22:29:19 -0600

    maping players

--- src/components /Playerlist.js
+++ src/components /Playerlist.js
@@ -2,16 +2,21 @@ import React from "react";
 import Player from "./Player";
 
 const PlayerList = (props) => {
-  const onePlayer = props.playerData[0];
+  const Players = props.playerData.map((player, key) => {
+    return (
+      <Player
+        fname={player.firstName}
+        lname={player.lastName}
+        gameTag={player.gameTag}
+        wins={player.wins}
+        gamerTag={key}
+      />
+    );
+  });
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player
-        fname={onePlayer.firstName}
-        lname={onePlayer.lastName}
-        gameTag={onePlayer.gameTag}
-        wins={onePlayer.wins}
-      />
+      {Players}
     </section>
   );
 };
