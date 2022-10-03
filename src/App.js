af41cf816e0ebdeda9099025898a0e0e128ac885
hugo-pb <h.palomerabecerra@gmail.com>
2022-10-02 18:27:35 -0600

    created data base mock

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
 import "./App.css";
+import MatchList from "./components /MatchList";
+import Playerlist from "./components /Playerlist";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>
+        Tourney Matches
+        <span>Where Coding and Tournaments found their Match!</span>
+      </h1>
+      <Playerlist />
+      <MatchList />
+    </div>
+  );
 }
 
 export default App;
--- src/components /MatchList.js
+++ src/components /MatchList.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Match from "./Match";
 
 const MatchList = () => {
   return (
     <section className="PlayerList MatchList">
       <h1>Match list</h1>
-      {/* Matches will be shown here */}
+      <Match />
     </section>
   );
 };
